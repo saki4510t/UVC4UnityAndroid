a96b40939509c7921a6fcc3f8d5bb3a2da352296
saki4510t <t_saki@serenegiant.com>
2019-12-08 09:21:45 +0900

    Cubeにタッチ操作用のスクリプトを追加(今はログに出力するだけ)

---
+++ Assets/Scripts/TouchHandler.cs
@@ -0,0 +1,31 @@
+﻿#define ENABLE_LOG
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchHandler : MonoBehaviour
+{
+	private TouchEventManager manager = new TouchEventManager();
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		manager.Update();
+
+		TouchEventManager.TouchEvent touch = manager.GetTouch();
+		if (touch.state == TouchEventManager.TouchState.Began)
+		{   // タッチした時
+#if (!NDEBUG && DEBUG && ENABLE_LOG)
+			Console.WriteLine("タッチした:");
+#endif
+		}
+	}
+}
