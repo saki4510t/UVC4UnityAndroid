43dd804e0db2c9814a896be0cccad827fb742c4a
saki4510t <t_saki@serenegiant.com>
2019-12-09 12:12:49 +0900

    タッチ操作をEventSystem/EventTriggerに変更

--- Assets/Scripts/CubeHandler.cs
+++ Assets/Scripts/CubeHandler.cs
@@ -5,47 +5,40 @@ using UnityEngine;
 
 using Serenegiant.UVC.Android;
 
-namespace Serenegiant {
-	public class CubeHandler : MonoBehaviour
+public class CubeHandler : MonoBehaviour
+{
+	private UVCController uvcController;
+	private Transform targetTransform;
+
+	// Start is called before the first frame update
+	void Start()
 	{
-		private TouchEventManager manager = new TouchEventManager();
-		private UVCController uvcController;
-		private Transform targetTransform;
+		uvcController = gameObject.GetComponent<UVCController>();
+		targetTransform = gameObject.transform;
+	}
 
-		// Start is called before the first frame update
-		void Start()
-		{
-			uvcController = gameObject.GetComponent<UVCController>();
-			targetTransform = gameObject.transform;
-		}
+	// Update is called once per frame
+	void Update()
+	{
 
-		// Update is called once per frame
-		void Update()
-		{
-			manager.Update();
+	}
 
-			TouchEventManager.TouchEvent touch = manager.GetTouch();
-			if (touch.state == TouchEventManager.TouchState.Began)
-			{   // タッチした時
-				OnClick();
-			}
+	void FixedUpdate()
+	{
+		targetTransform.Rotate(-0.2f, 0.1f, 0.05f);
+	}
 
-		}
-
-		void FixedUpdate()
-		{
-			targetTransform.Rotate(-0.2f, 0.1f, 0.05f);
-		}
-
-		public void OnClick()
-		{
+	/**
+		* オブジェクトにタッチしたときの処理
+		*/
+	public void OnClick()
+	{
 #if (!NDEBUG && DEBUG && ENABLE_LOG)
-			Console.WriteLine("OnClick:");
+		Console.WriteLine("OnClick:");
 #endif
-			if (uvcController != null)
-			{
-				uvcController.Toggle();
-			}
+		if (uvcController != null)
+		{
+			uvcController.Toggle();
 		}
 	}
-} // namespace Serenegiant
+}
