71b469b5094d05f69cc6af40c09ab0d2e4076a80
saki4510t <t_saki@serenegiant.com>
2019-12-16 07:52:25 +0900

    EventSystemでメッセージをJava側からと同じようにイベントメッセージを送れるようにインターフェースを追加

---
+++ Assets/UVC4UnityAndroidPlugin/Scripts/IUVCEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Serenegiant.UVC
+{
+
+	public interface IUVCEventHandler : IEventSystemHandler
+	{
+		void OnEventAttach(string args);
+		void OnEventPermission(string args);
+		void OnEventConnect(string args);
+		void OnEventDisconnect(string args);
+		void OnEventDetach(string args);
+		void OnEventReady(string args);
+		void OnStartPreview(string args);
+		void OnStopPreview(string args);
+
+	}   // IUVCEventHandler
+
+}	// namespace Serenegiant.UVC
