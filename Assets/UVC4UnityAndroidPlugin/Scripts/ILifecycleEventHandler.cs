f9fea8a7c13ceae3e536d42bc0f9201d4c17d05b
saki4510t <t_saki@serenegiant.com>
2019-12-18 00:10:03 +0900

    ライフサイクルとパーミッションのイベント用のインターフェースを追加

---
+++ Assets/UVC4UnityAndroidPlugin/Scripts/ILifecycleEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Serenegiant
+{
+	public interface ILifecycleEventHandler : IEventSystemHandler
+	{
+		void OnResume();
+		void OnPause();
+
+	} // ILifecycleEventHandler
+
+} // namespace Serenegiant
---
+++ Assets/UVC4UnityAndroidPlugin/Scripts/IPermissionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Serenegiant
+{
+	public interface IPermissionHandler : IEventSystemHandler
+	{
+		void OnGrant(string permission);
+		void OnDeny(string permission);
+		void OnDenyAndNeverAskAgain(string permission);
+
+	} // IPermissionHandler
+
+} // namespace Serenegiant
